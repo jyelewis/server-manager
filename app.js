ebf8efd21fb9e34c7c1c08d57a4d0ae57bc0fdb6
Jye Lewis <jye@jyelewis.com>
2015-01-28 01:11:30 -0500

    stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 
+app.use('/resources', express.static(__dirname + '/resources'));
 app.get('/', function (req, res) {
 	res.send('Hello World!')
 })
