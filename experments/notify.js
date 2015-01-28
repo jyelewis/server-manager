8318fab30c478d8f7721e2c5063fe1150fed8806
Jye Lewis <jye@jyelewis.com>
2015-01-29 09:40:38 +1100

    Testing notifications

---
+++ experments/notify.js
@@ -0,0 +1,24 @@
+var push = require( 'pushover-notifications' );
+
+var p = new push( {
+    user: "uf1ZaeJDC42zH13FJz2v1DHfbXdZgh",
+    token: "az4SBbUs4aCGG2XTBNGreUfP59pHgq",
+});
+
+var msg = {
+    // These values correspond to the parameters detailed on https://pushover.net/api
+    // 'message' is required. All other values are optional.
+    message: 'omg node test',   // required
+    title: "Well - this is fantastic",
+    sound: 'magic',
+    device: 'devicename',
+    priority: 1
+};
+
+p.send( msg, function( err, result ) {
+    if ( err ) {
+        throw err;
+    }
+
+    console.log( result );
+});
